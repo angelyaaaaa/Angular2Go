49f6d8f4f55ec98693bce90f2fe0deb4bbf3824d
angelyaaaaa <tsoen.526018@gmail.com>
2017-02-04 11:43:02 +0800

    事件繫結(event binding)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,13 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  pageTitle = 'Hello Willlll';
+  pageTitle = 'Hello Willlll+';
   homeUrl = 'http://blog.miniasp.com/';
   homeAlt = 'The Will Will Web----------';
+  count = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  plusOne(e: MouseEvent) {
+    console.log(e);
+    if(e.ctrlKey) {
+      this.count--;
+    } else {
+      this.count++;
+    }
+  }
+
+  copyRightWarning(e) {
+    alert('this is XXX right');
+  }
 }
