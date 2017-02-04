66b54d80f8070cc4c194d997b3fc914303c7953e
angelyaaaaa <tsoen.526018@gmail.com>
2017-02-04 11:17:14 +0800

    示範資料繫結(data binding) 與 中括號的屬性繫結(property binding)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  pageTitle = 'Hello Willlll';
+  homeUrl = 'http://blog.miniasp.com/';
+  homeAlt = 'The Will Will Web----------';
 
   constructor() { }
 
